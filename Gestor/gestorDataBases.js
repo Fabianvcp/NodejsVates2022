eeb192cce25a24b731271cbfd73d6b832ead17df
Fabianvcpz <gallardofabianvcpz@gmail.com>
2022-06-25 20:23:50 -0300

    ABM base de telefono listo

--- Gestor/gestorDataBases.js
+++ Gestor/gestorDataBases.js
@@ -4,12 +4,32 @@ const mariadb = require('mariadb');
 const config = require("../Config/bd");
 
 //ABM de Telefonos
-async function agregarLinea(nueva) {
+async function verificar(comprovacion) {
     const conn = await mariadb.createConnection(config)
-    const linea = [nueva.id_telefono,nueva.numero,nueva.id_tipo,nueva.documento]
-    await conn.query("INSERT INTO telefonos(id_telefono, numero, id_tipo, documento) value(?,?,?,?)",linea)
+    let linea = await conn.query("SELECT COUNT(*) as resultado FROM telefonos WHERE numero = ?", [comprovacion]);    
+    let resultado = parseInt(linea[0].resultado)
+    console.log(resultado);
     conn.end()
+    return resultado
 }
+//agragar nueva linea
+async function agregarLinea(nuevaLinea) {
+    const conn = await mariadb.createConnection(config)
+    let comprovacion = [nuevaLinea.numero]       
+    let linea =await verificar(comprovacion);
+
+    if (linea == 0 ) {   
+        const agregar = [nuevaLinea.numero,nuevaLinea.id_tipo,nuevaLinea.documento];
+        await conn.query("INSERT INTO telefonos( numero, id_tipo, documento) value(?,?,?)",agregar);
+    } else { 
+        conn.end()           
+        return false;
+    }
+   
+    conn.end()         
+    return true
+}
+//consultar todas las lineas
 async function consultarTodasLasLineas() {
     const conn = await mariadb.createConnection(config)
     let lineas = await conn.query("SELECT * FROM telefonos")
@@ -17,19 +37,30 @@ async function consultarTodasLasLineas() {
     return lineas
 
 }
+//consultar linea
 async function consultarLinea(num) {
     const conn = await mariadb.createConnection(config)
     let linea = await conn.query("SELECT * FROM telefonos WHERE id_telefono = ?", [num])
     conn.end()
     return linea[0]
 }
+//editar linea 
 async function editarLinea(linea) {
     const conn = await mariadb.createConnection(config)
-    let valores = [linea.id_telefono, linea.numero, linea.id_tipo, linea.documento]
-    // await conn.query()
+    let comprovacion = parseInt([linea.numero])     
+    let encontrada =await verificar(comprovacion);
+    if (encontrada) {            
+        const modificar = [ linea.numero, linea.id_tipo, linea.documento,linea.id_telefono];            
+        await conn.query("UPDATE telefonos SET numero = ?,id_tipo= ?,documento=? WHERE id_telefono = ?",modificar);
+    } else {  
+        console.log("Falla el if del encontrado");          
+        conn.end()           
+        return false;
+    }
     conn.end()
-    return valores
+    return true
 }
+//Eliminar linea
 async function eliminarLinea(num) {
     const conn = await mariadb.createConnection(config)
     let linea = await conn.query("SELECT * FROM telefonos WHERE id_telefono = ?", [num])
--- Routers/routerstelefonos.js
+++ Routers/routerstelefonos.js
@@ -2,30 +2,29 @@ const express = require('express')
 const gestor = require('../Gestor/gestorDataBases')
 const router = express.Router()
 
+
 router.use(express.json())
 
 //ABM de Telefono
 // * post crear linea
 router.post("/", async(req, res)=>{
-    let nuevaLinea = req.body
-    let linea = nuevaLinea['id_telefono']
-    let telefono_encontrado = await gestor.consultarLinea(linea)
-    
-    if(telefono_encontrado){
-        res.sendStatus(406).send("El valor ya existe en la base de datos")
+    let nuevaLinea = req.body    
+    let agregado = await gestor.agregarLinea(nuevaLinea)
+    if(agregado){
+        res.status(200).send("Linea insertada")
     }else{
-        await gestor.agregarLinea(nuevaLinea)
-        res.sendStatus(201).send("Linea insertada")
-
+        res.status(400).send("El valor ya existe en la base de datos")
     }
     res.end()
 
 })
+
 //Traer todas las lineas
-router.get("/", async(req, res)=>{
-    res.json(await gestor.consultarTodasLasLineas)
+router.get("/", async(req, res) => {
+    res.json(await  gestor.consultarTodasLasLineas())
     res.end()
 })
+
 //traer solo la linea solicitada
 router.get("/:linea", async (req, res) => {
     let num = parseInt(req.params.linea)
@@ -47,16 +46,19 @@ router.get("/:linea", async (req, res) => {
 //editar datos de la linea
 router.put("/:linea", async(req, res)=>{
     let linea = parseInt(req.params.linea)    
-    if (!isNaN(num)) {
+    if (!isNaN(linea)) {
         let lineaNueva = req.body
-        lineaNueva.id_telefono = num
-        let linea_encontrada  = await gestor.consultarLinea(num)
-
+        lineaNueva.id_telefono = linea
+        let linea_encontrada  = await gestor.consultarLinea(linea)
         if (linea_encontrada) {
-            await gestor.editarLinea(lineaNueva)
-            res.sendStatus(200).send("La linea fue editada")
+            let linea_editada = await gestor.editarLinea(lineaNueva)
+            if (linea_editada) {
+                res.status(200).send("La linea fue editada")
+            } else {                
+                res.status(400).send("Error en la edición")
+            }
         } else {
-            res.status(400).send("La linea ingresada no existe")
+            res.status(404).send("La linea ingresada no existe")
         }
     } else {
         res.status(400).send("El parámetro debe ser numérico")
@@ -89,3 +91,6 @@ router.delete("/:linea", async(req, res)=>{
 // Relación entre personas y telefonos
 
 // Búsquedas
+
+
+exports.router = router
